8e72a838e989b6161513e85de8a304c9ef2765dc
Esteban Villantoy <esteban.villantoy@touchconsulting.pe>
2025-02-19 13:16:43 -0500

    feat: nuestro servicios

---
+++ src/types/home/nuestroServicios.d.ts
@@ -0,0 +1,63 @@
+export interface NuestroServiciosType {
+    id: number;
+    documentId: string;
+    createdAt: Date;
+    updatedAt: Date;
+    publishedAt: Date;
+    Contenido: Contenido[];
+}
+
+export interface Contenido {
+    __component: string;
+    id: number;
+    credito_imagen: Imagen;
+    ahorros_imagen: Imagen;
+}
+
+export interface Imagen {
+    id: number;
+    documentId: string;
+    name: string;
+    alternativeText: null;
+    caption: null;
+    width: number;
+    height: number;
+    formats: Formats;
+    hash: string;
+    ext: string;
+    mime: string;
+    size: number;
+    url: string;
+    previewUrl: null;
+    provider: string;
+    provider_metadata: ProviderMetadata;
+    createdAt: Date;
+    updatedAt: Date;
+    publishedAt: Date;
+}
+
+export interface Formats {
+    large: Large;
+    small: Large;
+    medium: Large;
+    thumbnail: Large;
+}
+
+export interface Large {
+    ext: string;
+    url: string;
+    hash: string;
+    mime: string;
+    name: string;
+    path: null;
+    size: number;
+    width: number;
+    height: number;
+    sizeInBytes: number;
+    provider_metadata: ProviderMetadata;
+}
+
+export interface ProviderMetadata {
+    public_id: string;
+    resource_type: string;
+}
